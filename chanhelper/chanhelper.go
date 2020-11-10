2f6ef0838813181357587dee456f5b375340c53a
Oleg Subachev <solsw@posolsoft.com>
2020-11-10 22:00:10 +0500

    2020.11.10-2

--- chanhelper/chanhelper.go
+++ chanhelper/chanhelper.go
@@ -1,9 +1,12 @@
 package chanhelper
 
+import (
+	"sync"
+)
+
 // Merge2Ints merges two int channels into one.
 func Merge2Ints(in1, in2 <-chan int) <-chan int {
 	out := make(chan int)
-
 	go func(in1, in2 <-chan int, out chan<- int) {
 		var (
 			v   int
@@ -24,6 +27,30 @@ func Merge2Ints(in1, in2 <-chan int) <-chan int {
 		}
 		close(out)
 	}(in1, in2, out)
+	return out
+}
 
+// MergeNInts merges several int channels into one.
+func MergeNInts(ins ...chan int) <-chan int {
+	if len(ins) == 0 {
+		return nil
+	}
+	// https://stackoverflow.com/questions/19992334/how-to-listen-to-n-channels-dynamic-select-statement/32381409#32381409
+	// https://play.golang.org/p/v4M14CL7OL
+	out := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(len(ins))
+	for _, c := range ins {
+		go func(c <-chan int) {
+			for v := range c {
+				out <- v
+			}
+			wg.Done()
+		}(c)
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
 	return out
 }
--- chanhelper/chanhelper_test.go
+++ chanhelper/chanhelper_test.go
@@ -11,21 +11,18 @@ func TestMerge2Ints(t *testing.T) {
 		in1 chan int = make(chan int)
 		in2 chan int = make(chan int)
 	)
-
 	go func() {
 		for i := 0; i < 4; i++ {
 			in1 <- 1
 		}
 		close(in1)
 	}()
-
 	go func() {
 		for i := 0; i < 4; i++ {
 			in2 <- 2
 		}
 		close(in2)
 	}()
-
 	var out []int
 	for v := range Merge2Ints(in1, in2) {
 		out = append(out, v)
@@ -33,6 +30,27 @@ func TestMerge2Ints(t *testing.T) {
 	sort.Ints(out)
 	want := []int{1, 1, 1, 1, 2, 2, 2, 2}
 	if !reflect.DeepEqual(out, want) {
-		t.Errorf("MergeInts() = %v, want %v", out, want)
+		t.Errorf("Merge2Ints() = %v, want %v", out, want)
+	}
+}
+
+func TestMergeNInts(t *testing.T) {
+	cc := []chan int{make(chan int), make(chan int), make(chan int), make(chan int)}
+	for i, c := range cc {
+		go func(i int, c chan<- int) {
+			for n := 0; n < 2; n++ {
+				c <- i
+			}
+			close(c)
+		}(i, c)
+	}
+	var out []int
+	for v := range MergeNInts(cc...) {
+		out = append(out, v)
+	}
+	sort.Ints(out)
+	want := []int{0, 0, 1, 1, 2, 2, 3, 3}
+	if !reflect.DeepEqual(out, want) {
+		t.Errorf("MergeNInts() = %v, want %v", out, want)
 	}
 }

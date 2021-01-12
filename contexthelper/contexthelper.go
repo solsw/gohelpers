2b6841b205d24198ab0fe6d10e964ef9291be9cd
Oleg Subachev <solsw@posolsoft.com>
2021-01-12 10:35:01 +0500

    2021.01.12

--- contexthelper/contexthelper.go
+++ contexthelper/contexthelper.go
@@ -1,3 +1,4 @@
+// Package contexthelper contains context helpers.
 package contexthelper
 
 import (
@@ -7,7 +8,7 @@ import (
 )
 
 // WithTerminate returns context.Context
-// that is canceled when os.Interrupt or os.Kill is received.
+// that is canceled when os.Interrupt or os.Kill signal is received.
 func WithTerminate(ctx context.Context) context.Context {
 	// inspired by https://gist.github.com/SpeedyCoder/59911301eea4d91e42feafccaa9bcaf7
 	ctx, cancel := context.WithCancel(ctx)

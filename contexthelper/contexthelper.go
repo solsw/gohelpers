0228d376b37a1278581d8d59a2f72559ce9e8c9b
Oleg Subachev <solsw@posolsoft.com>
2021-05-20 14:57:11 +0500

    v0.5.0

--- contexthelper/contexthelper.go
+++ contexthelper/contexthelper.go
@@ -3,23 +3,7 @@ package contexthelper
 
 import (
 	"context"
-	"os"
-	"os/signal"
 )
-
-// WithTerminate returns context.Context
-// that is canceled when os.Interrupt or os.Kill signal is received.
-func WithTerminate(ctx context.Context) context.Context {
-	// inspired by https://gist.github.com/SpeedyCoder/59911301eea4d91e42feafccaa9bcaf7
-	ctx, cancel := context.WithCancel(ctx)
-	go func() {
-		sigch := make(chan os.Signal, 1)
-		signal.Notify(sigch, os.Interrupt, os.Kill)
-		<-sigch
-		cancel()
-	}()
-	return ctx
-}
 
 // BoolValue returns the bool value associated with this context for 'key'
 // and a bool indicating whether the value exists and is of bool type.
--- contexthelper/contexthelper_test.go
+++ contexthelper/contexthelper_test.go
@@ -20,9 +20,30 @@ func TestStringValue(t *testing.T) {
 		want   string
 		wantok bool
 	}{
-		{name: "1", args: args{ctx: context.WithValue(context.Background(), key1, key1), key: key1}, want: key1, wantok: true},
-		{name: "2", args: args{ctx: context.WithValue(context.Background(), key1, key1), key: key2}, want: "", wantok: false},
-		{name: "3", args: args{ctx: context.WithValue(context.Background(), key1, 1), key: key1}, want: "", wantok: false},
+		{name: "1",
+			args: args{
+				ctx: context.WithValue(context.Background(), key1, key1),
+				key: key1,
+			},
+			want:   key1,
+			wantok: true,
+		},
+		{name: "2",
+			args: args{
+				ctx: context.WithValue(context.Background(), key1, key1),
+				key: key2,
+			},
+			want:   "",
+			wantok: false,
+		},
+		{name: "3",
+			args: args{
+				ctx: context.WithValue(context.Background(), key1, 1),
+				key: key1,
+			},
+			want:   "",
+			wantok: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {

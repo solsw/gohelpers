ada60c219682e37e13a0aa574125dd3ce4e1ecbd
Oleg Subachev <solsw@posolsoft.com>
2021-07-09 10:43:35 +0500

    v0.9.0

--- iohelper/asyncwriter.go
+++ iohelper/asyncwriter.go
@@ -2,24 +2,24 @@ package iohelper
 
 import (
 	"io"
-	"math"
 )
 
 // AsyncWriter is the io.WriteCloser interface implementation, that does not block on Write call.
 type AsyncWriter struct {
-	wr io.Writer
-	ch chan []byte
-	dn chan struct{}
-	nb int
-	er error
+	writer    io.Writer
+	ch        chan []byte
+	done      chan struct{}
+	isWriting bool
+	nbytes    int
+	er        error
 }
 
-// NewAsyncWriterSizeFunc returns a new AsyncWriter with 'size' capacity of the underlying channel.
+// NewAsyncWriter returns a new AsyncWriter with 'size' capacity of the underlying channel.
 // 'f' (if not nil) is used to process bytes before writing.
-func NewAsyncWriterSizeFunc(w io.Writer, size int, f func([]byte) []byte) *AsyncWriter {
-	r := AsyncWriter{wr: w, ch: make(chan []byte, size), dn: make(chan struct{})}
+func NewAsyncWriter(w io.Writer, size int, f func([]byte) []byte) *AsyncWriter {
+	newaw := AsyncWriter{writer: w, ch: make(chan []byte, size), done: make(chan struct{})}
 	go func(aw *AsyncWriter) {
-		defer func() { aw.dn <- struct{}{} }()
+		defer func() { aw.done <- struct{}{} }()
 		for bb := range aw.ch {
 			if aw.er != nil {
 				return
@@ -27,57 +27,44 @@ func NewAsyncWriterSizeFunc(w io.Writer, size int, f func([]byte) []byte) *Async
 			if f != nil {
 				bb = f(bb)
 			}
-			aw.nb, aw.er = aw.wr.Write(bb)
+			aw.isWriting = true
+			aw.nbytes, aw.er = aw.writer.Write(bb)
+			aw.isWriting = false
 		}
-	}(&r)
-	return &r
-}
-
-// NewAsyncWriterSize returns a new AsyncWriter with 'size' capacity of the underlying channel
-// and no processing function (see NewAsyncWriterSizeFunc).
-func NewAsyncWriterSize(w io.Writer, size int) *AsyncWriter {
-	return NewAsyncWriterSizeFunc(w, size, nil)
-}
-
-// NewAsyncWriterFunc returns a new AsyncWriter with math.MaxInt16 capacity of the underlying channel
-// and 'f' processing function (see NewAsyncWriterSizeFunc).
-func NewAsyncWriterFunc(w io.Writer, f func([]byte) []byte) *AsyncWriter {
-	return NewAsyncWriterSizeFunc(w, math.MaxInt16, f)
-}
-
-// NewAsyncWriter returns a new AsyncWriter with math.MaxInt16 capacity of the underlying channel
-// and no processing function (see NewAsyncWriterSizeFunc).
-func NewAsyncWriter(w io.Writer) *AsyncWriter {
-	return NewAsyncWriterSizeFunc(w, math.MaxInt16, nil)
+	}(&newaw)
+	return &newaw
 }
 
 // Write implements the io.Writer interface.
-// Write returns len(p) and nil error.
+// Write always returns len(p) and nil.
 func (aw *AsyncWriter) Write(p []byte) (int, error) {
 	// since the same slice may be passed to this method in separate calls (e.g. as log.Println does),
 	// the current contents of 'p' must be copied to new local slice
-	// (fmt.Printf("%p\n", p) <- prints the same address when AsyncWriter is passed to log.SetOutput)
+	// fmt.Printf("%p\n", p) <- prints the same address when AsyncWriter is passed to log.SetOutput
 	loc := make([]byte, len(p))
-	// (fmt.Printf("%p\n", loc) <- prints different addresses)
+	// fmt.Printf("%p\n", loc) <- prints different addresses
 	copy(loc, p)
 	aw.ch <- loc
 	return len(loc), nil
 }
 
-// Result returns result of the last Write call on the underlying io.Writer.
-func (aw *AsyncWriter) Result() (int, error) {
-	return aw.nb, aw.er
+// IsWriting reports whether the underlying io.Writer is in the writing phase or not.
+func (aw *AsyncWriter) IsWriting() bool {
+	return aw.isWriting
 }
 
-// Close closes the underlying channel.
-// Close must be called (typically by "defer" statement) to wait for the underlying io.Writer to finish writing.
-// If the underlying io.Writer is io.Closer, Close calls its Close method
-// and returns corresponding result error, otherwise Close returns nil.
+// Result returns result of the last completed Write call on the underlying io.Writer.
+func (aw *AsyncWriter) Result() (int, error) {
+	return aw.nbytes, aw.er
+}
+
+// Close closes AsyncWriter by closing the underlying channel and waiting for the underlying io.Writer to finish writing.
+// If the underlying io.Writer is io.Closer, it is closed and the result is returned, otherwise Close returns nil.
 // Close implements the io.Closer interface.
 func (aw *AsyncWriter) Close() error {
 	close(aw.ch)
-	<-aw.dn
-	if cl, ok := aw.wr.(io.Closer); ok {
+	<-aw.done
+	if cl, ok := aw.writer.(io.Closer); ok {
 		return cl.Close()
 	}
 	return nil
